e42668e096f843353b7f382d1f654afbc3a89ae1
clement-papia <clement.papia@laplateforme.io>
2023-03-01 08:27:21 +0100

    rendu runtrack
    évolution job 4.

---
+++ Runctrack-poo-jour2/Job4.py
@@ -0,0 +1,17 @@
+class Student:
+    def __init__(self,prenom,nom,numéroétudiant,nombredecrédit = 0):
+        self.__nom = nom
+        self.__prenom = prenom
+        self.__numéroétudiant = numéroétudiant
+        self.__nombredecrédit = nombredecrédit
+    
+    def add_credits(self,credits):
+        if credits>0:
+            self.__nombredecrédit += credits
+            print("Le nombre de credits de John Doe est de",credits)
+
+    def ___Studenteval(self):
+        
+
+eleve = Student("John","Doe",145)
+eleve.add_credits(60)
